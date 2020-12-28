62978813c0d8b18bd15fba672b57a24166f9fc22
TatsuyaM <tat.mae084@gmail.com>
2020-12-29 00:13:51 +0900

    day4追加

---
+++ day4/Exercise401/Exercise401/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Exercise401
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r1 = new Random();
+            //Random num2 = new Random();
+
+            int num1 = r1.Next(1, 11);
+            int num2 = r1.Next(1, 11);
+
+            Console.WriteLine("a=" + num1 + " b=" + num2 +" = "+ (num1+num2) );
+            int answer = int.Parse(Console.ReadLine());
+
+            if (answer == num1+num2)
+            {
+                Console.WriteLine("正解");
+            }
+            else
+            {
+                Console.WriteLine("まちがい");
+            }
+
+
+            Console.WriteLine("数値を入力してください");
+            int num = int.Parse( Console.ReadLine());
+            int count = 1;
+
+            while (count < num)
+            {
+                if (num % count == 0 && num == count)
+                {
+                    Console.Write(count);
+                }
+                else if (num % count == 0 )
+                {
+                    Console.Write(count + " ");
+                }
+
+                count++;
+            }
+
+
+
+
+            
+
+            for (int i = 1; i <= 10; i++)
+            {
+                //Console.WriteLine(i+"Hello World!");
+            }
+        }
+    }
+}
--- day4/Sample401/Sample401/Program.cs
+++ day4/Sample401/Sample401/Program.cs
@@ -6,15 +6,32 @@ namespace Sample401
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //while と Do Whileの違い
+            //while:もし１回目から条件式がfalseだとしたら計算は実行されない
+            //Do while:最低１回は計算処理は実行される
+
+
+            //304
+            int cnt = 0;
+
+            while (cnt < 5)
+            {
+                Console.WriteLine(cnt);
+                cnt++;
+            }
+
+
+
+            Console.WriteLine("※※※※※※※※※※※※※※※※※※※※※※※※");
+
             int i_max = 3;
             int k_max = 4;
 
-            for (int i = 0; i < i_max; i++)
-            {
-                for (int k = 1; k < k_max; k++)
-                {
-                    Console.Write(i + " + " + k + " = " + i + k);
+            for (int i = 1; i < i_max; i++){
+
+                for (int k = 1; k < k_max; k++){
+                    Console.Write(i + " + " + k + " = " + (i + k) +" / ");
                 }
                 Console.WriteLine("");
             }
