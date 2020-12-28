46e89c7ef623588f4dce906ad3deecddcc84dd43
TatsuyaM <tat.mae084@gmail.com>
2020-12-28 15:31:45 +0900

    ファイル変更

---
+++ day4/Sample401/Sample401/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sample401
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i_max = 3;
+            int k_max = 4;
+
+            for (int i = 0; i < i_max; i++)
+            {
+                for (int k = 1; k < k_max; k++)
+                {
+                    Console.Write(i + " + " + k + " = " + i + k);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
