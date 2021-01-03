4d88d15d2d6223fb6b5fe1e10aeb0522cf5c4beb
TatsuyaM <tat.mae084@gmail.com>
2021-01-03 21:45:33 +0900

    add files

--- day13_コレクション/SampleEx601/SampleEx601/Program.cs
+++ day13_コレクション/SampleEx601/SampleEx601/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SampleEx601
 {
@@ -6,7 +7,17 @@ namespace SampleEx601
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> a = new List<int>();
+            //  値を順に挿入
+            a.Add(3);
+            a.Add(2);
+            a.Add(1);
+            //  1番目に4を挿入
+            a.Insert(1, 4);
+            for (int i = 0; i < a.Count; i++)
+            {
+                Console.WriteLine("a[{0}]={1} ", i, a[i]);
+            }
         }
     }
 }
